90a1d0bb7a9427c64a188bf05f9295fc3f6c7b66
Hiperblade <hiperblade.2501@gmail.com>
2014-11-07 07:13:07 +0100

    Update release 4.0

--- platforms/android/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-/home/hiperblade/Documents/Cordova/cocktails/platforms/android/gen/com/hiperblade/cocktails/R.java \
- : /home/hiperblade/Documents/Cordova/cocktails/platforms/android/res/drawable-ldpi/icon.png \
-/home/hiperblade/Documents/Cordova/cocktails/platforms/android/res/drawable/icon.png \
-/home/hiperblade/Documents/Cordova/cocktails/platforms/android/res/values/strings.xml \
-/home/hiperblade/Documents/Cordova/cocktails/platforms/android/res/drawable-mdpi/icon.png \
-/home/hiperblade/Documents/Cordova/cocktails/platforms/android/res/drawable-xhdpi/icon.png \
-/home/hiperblade/Documents/Cordova/cocktails/platforms/android/res/drawable-hdpi/icon.png \
-/home/hiperblade/Documents/Cordova/cocktails/platforms/android/res/xml/config.xml \
-/home/hiperblade/Documents/Cordova/cocktails/platforms/android/bin/AndroidManifest.xml \
